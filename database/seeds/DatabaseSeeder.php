ad330767d5eb7882b7eace882bd63d89f64483e5
Alexander Bunzya <a.bunzya@gmail.com>
2016-08-31 08:52:43 +0300

    fixed seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(GroupTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(ChatTableSeeder::class);
     }
 }
