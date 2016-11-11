10e9c7b7c9e2f228f47f381d82b769f24acd5d5d
Alexander Bunzya <a.bunzya@gmail.com>
2016-11-11 11:21:49 +0300

    inser plane text

--- public/apps/chatApp/directives/controlCaret/controlCaret.js
+++ public/apps/chatApp/directives/controlCaret/controlCaret.js
@@ -82,8 +82,8 @@
             element.on('paste',function(e) {
                 e.preventDefault();
                 setCaret(sel.focusNode, sel.focusOffset);
-                var text = (e.originalEvent || e).clipboardData.getData('text/html') ;
-                var $result = $('<div></div>').append((text));
+                var text = (e.originalEvent || e).clipboardData.getData('text/plain') ;
+               /* var $result = $('<div></div>').append((text));
                 $.each($result.find("*"), function(idx, val) {
 
                     var $item = $(val);
@@ -105,8 +105,8 @@
 
                 $result = $result.html().replace(/<(span|hr|font|strong|blockquote|a|h3|h2|h1|h4|h5|h6|b|img|input|i|u|sub|sup|strike|small|s|pre|font|li|ul)[^>]+>/ig,'');
 
-                //$(this).html($result);
-                pasteHtmlAtCaret($result);
+                //$(this).html($result);*/
+                pasteHtmlAtCaret(text);
                 sel = rangy.getSelection();
                 setCaret(sel.focusNode, sel.focusOffset);
                 sel.focusNode.focus();
